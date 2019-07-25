dc2124ad34859ee7f4c6d23140486a9977086892
Dragon8029 <dragon8029@gmail.com>
2019-07-25 06:41:40 -0700

    fix bugs

--- client/app.js
+++ client/app.js
@@ -2,10 +2,11 @@ var socket = io();
 
 $('form').on('submit', function () {
     var text = $('#message').val();
-    socket.emit('message', text);
+   
     $('#message').val('');
     return false;
 });
+
 
 socket.on('message', function (msg) {
     $('<li>').text(msg).appendTo('#history');
